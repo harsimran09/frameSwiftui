2a452fbe2fa424cad497dc5bc9e2c03122c3b051
Harsimran Kaur <harsimran.kaur1@whitehatjr.com>
2022-05-26 20:10:51 +0530

    Initial Commit

---
+++ frame1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  frame1
+//
+//  Created by ITRS-1519 on 26/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ frame1/frame1App.swift
@@ -0,0 +1,17 @@
+//
+//  frame1App.swift
+//  frame1
+//
+//  Created by ITRS-1519 on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct frame1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
